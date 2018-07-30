5008c47c2061ddeed33e2d00f7bd22a33197e4a6
Jamiel Sheikh <i@jamiel.net>
2018-07-29 22:44:09 -0400

    ex24

---
+++ ex24-bitfinex.js
@@ -0,0 +1,65 @@
+var WebSocketClient = require('websocket').client;
+
+// 1 npm install gdax
+
+async function run() {    
+
+    
+    conbaseProRequest = `
+    {
+        "type": "subscribe",
+        "product_ids": [
+            "ETH-USD"
+        ],
+        "channels": [
+            "level2",
+            "ticker",
+            {
+                "name": "ticker",
+                "product_ids": [
+                    "ETH-USD"
+                ]
+            }
+        ]
+    }`
+    
+    
+    
+    bitfinexRequest = `
+    {
+        "event":"subscribe",
+        "channel":"book",
+        "symbol":"ETHUSD"
+     }
+     `
+
+    coinbaseProClient = new WebSocketClient();
+    
+   
+    coinbaseProClient.on('connect', function(conn) {
+        console.log('Connected');
+        conn.on('message', function(message) {
+            console.log(message);
+            
+            // 6
+            console.log(JSON.parse(message.utf8Data).changes);
+        });
+        conn.sendUTF(conbaseProRequest);
+    });
+    coinbaseProClient.connect('wss://ws-feed.gdax.com');
+
+    bitfinexClient = new WebSocketClient();
+    bitfinexClient.on('connect', function(conn) {
+        console.log('Connected');
+        conn.on('message', function(message) {
+            console.log(message);
+        });
+        conn.sendUTF(bitfinexRequest);
+    });
+
+    bitfinexClient.connect('wss://api.bitfinex.com/ws');
+
+};
+
+
+run();
